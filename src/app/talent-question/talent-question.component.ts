2ff3f514f67222a845434167a159c938a634f954
Sameer Shukla <sameershukla1999@gmail.com>
2022-07-21 20:58:21 +0530

    few more changes

--- src/app/talent-question/talent-question.component.ts
+++ src/app/talent-question/talent-question.component.ts
@@ -208,58 +208,19 @@ export class TalentQuestionComponent implements OnInit {
           let dataValues = []; //For values
           for (let key in this.rootTalentObject.talentProfile) {
             dataValues.push(this.rootTalentObject.talentProfile[key]);
-            this.dataKeys.push({
-              value: this.rootTalentObject.talentProfile[key]['Primary Domain'],
-              selected: false,
-              disable: false,
-              noOfYear: '',
-              skill: '',
-            });
+            this.dataKeys.push({value: this.rootTalentObject.talentProfile[key]['Primary Domain'],selected: false, disable: false,noOfYear: '',skill: '',});
             for (let key2 in this.rootTalentObject.talentProfile[key].Modules) {
-              this.datathree.push({
-                value: key2,
-                selected: false,
-                disable: false,
-                noOfYear: '',
-                skill: '',
-                group:
-                  this.rootTalentObject.talentProfile[key]['Primary Domain'],
-              });
-              this.rootTalentObject.talentProfile[key].Modules[key2][
-                'Job roles'
-              ].forEach((jobdata: any) => {
+              this.datathree.push({value: key2,selected: false,disable: false,noOfYear: '',skill: '', group:this.rootTalentObject.talentProfile[key]['Primary Domain']});
+              this.rootTalentObject.talentProfile[key].Modules[key2]['Job roles'].forEach((jobdata: any) => {
                 if (jobdata) {
-                  this.completeJobs.push({
-                    value: jobdata,
-                    selected: false,
-                    disable: false,
-                    noOfYear: '',
-                    skill: '',
-                    group: key,
-                    subgroup: key2,
-                    primaryDomain:
-                      this.rootTalentObject.talentProfile[key][
-                      'Primary Domain'
-                      ],
+                  this.completeJobs.push({value: jobdata,selected: false,disable: false,noOfYear: '',skill: '',group: key,subgroup: key2,primaryDomain:this.rootTalentObject.talentProfile[key]['Primary Domain'],
                   });
                 }
               });
-              this.rootTalentObject.talentProfile[key].Modules[
-                key2
-              ].Product.forEach((productdata: any) => {
+              this.rootTalentObject.talentProfile[key].Modules[key2].Product.forEach((productdata: any) => {
                 if (productdata) {
-                  this.completeProduct.push({
-                    value: productdata.name,
-                    selected: false,
-                    disable: false,
-                    noOfYear: '',
-                    skill: '',
-                    group: key,
-                    subgroup: key2,
-                    primaryDomain:
-                      this.rootTalentObject.talentProfile[key][
-                      'Primary Domain'
-                      ],
+                  this.completeProduct.push({value: productdata.name,selected: false,disable: false,noOfYear: '', skill: '', group: key,subgroup: key2,
+                    primaryDomain:this.rootTalentObject.talentProfile[key]['Primary Domain'],
                   });
                 }
               });
@@ -554,38 +515,38 @@ export class TalentQuestionComponent implements OnInit {
     }
   }
 
-  sliceItem(event: string) {
-    this.removeItem(event);
-    if (this.currentPage == 2) {
-      this.dyanmicOption.splice(this.dyanmicOption.indexOf(event), 1);
-    }
-  }
+  // sliceItem(event: string) {
+  //   this.removeItem(event);
+  //   if (this.currentPage == 2) {
+  //     this.dyanmicOption.splice(this.dyanmicOption.indexOf(event), 1);
+  //   }
+  // }
 
-  removeItem(option: any) {
-    if (this.fieldArray.length) {
-      for (var i = 0; i < this.fieldArray.length; i = i + 1) {
-        if (this.fieldArray[i].value === option.value) {
-          this.fieldArray.splice(i, 1);
-        }
-      }
-    }
-    if (this.savePreviousData.length) {
-      for (var i = 0; i < this.savePreviousData.length; i = i + 1) {
-        if (this.savePreviousData[i].value === option.value) {
-          this.savePreviousData.splice(i, 1);
-        }
-      }
-    }
-    let savedData =
-      this.talentProfile.stepObject[this.currentPage - 1].optionObj[0];
-    if (savedData.length) {
-      for (var i = 0; i < savedData.length; i = i + 1) {
-        if (savedData[i].value === option.value) {
-          savedData.splice(i, 1);
-        }
-      }
-    }
-  }
+  // removeItem(option: any) {
+  //   if (this.fieldArray.length) {
+  //     for (var i = 0; i < this.fieldArray.length; i = i + 1) {
+  //       if (this.fieldArray[i].value === option.value) {
+  //         this.fieldArray.splice(i, 1);
+  //       }
+  //     }
+  //   }
+  //   if (this.savePreviousData.length) {
+  //     for (var i = 0; i < this.savePreviousData.length; i = i + 1) {
+  //       if (this.savePreviousData[i].value === option.value) {
+  //         this.savePreviousData.splice(i, 1);
+  //       }
+  //     }
+  //   }
+  //   let savedData =
+  //     this.talentProfile.stepObject[this.currentPage - 1].optionObj[0];
+  //   if (savedData.length) {
+  //     for (var i = 0; i < savedData.length; i = i + 1) {
+  //       if (savedData[i].value === option.value) {
+  //         savedData.splice(i, 1);
+  //       }
+  //     }
+  //   }
+  // }
 
   selectOption(opt: any, type?: any) {
     console.log(this.talentQuestionData);
@@ -596,15 +557,15 @@ export class TalentQuestionComponent implements OnInit {
       this.enterpriseApplicationGroup = opt.value;
       this.disableOtherValues(opt);
       this.getDataForFieldArray(opt);
-    } else if (
-      this.currentPage === 3 ||
-      this.currentPage === 4 ||
-      this.currentPage === 5 ||
-      this.currentPage === 6
-    ) {
-      if (this.currentPage === 3) {
-        this.enterpriseApplicationSubGroup.push(opt.value);
-      }
+
+    }
+     else if(this.currentPage === 3){
+       this.enterpriseApplicationSubGroup.push(opt.value);
+       this.getDisableForQue3(opt);
+       this.getDataForFieldArray(opt);
+
+    }
+    else if (this.currentPage === 4 || this.currentPage === 5 || this.currentPage === 6) {
       opt.selected = true;
       opt.disable = true;
       this.getDataForFieldArray(opt);
@@ -718,7 +679,7 @@ export class TalentQuestionComponent implements OnInit {
   }
 
   disableOtherValues(opt: any) {
-    if (this.currentPage === 2 || this.currentPage === 6) {
+    if (this.currentPage === 2 || this.currentPage === 6 || this.currentPage === 3) {
       this.talentQuestionData[this.currentPage].option.forEach(
         (element: any) => {
           element.disable = true;
@@ -737,9 +698,9 @@ export class TalentQuestionComponent implements OnInit {
     return count;
   }
 
-  deleteFieldValue(option: any) {
-    this.sliceItem(option);
-  }
+  // deleteFieldValue(option: any) {
+  //   this.sliceItem(option);
+  // }
 
   onInputNoOfYear(opt: any) {
     if (this.currentPage === 2) {
@@ -840,60 +801,75 @@ export class TalentQuestionComponent implements OnInit {
   }
   deleteSelectedOption(field: any) {
     if (this.currentPage === 2) {
-      this.talentQuestionData[this.currentPage].option.forEach(
-        (element: any) => {
+      this.talentQuestionData[this.currentPage].option.forEach((element: any) => {
           element.disable = false;
           if (element.value.toLowerCase() === field.value.toLowerCase()) {
             element.selected = false;
             element.noOfYear = element.skill = '';
-            this.fieldArray = this.fieldArray.filter(
-              (item: any) =>
-                item.value.toLowerCase() !== field.value.toLowerCase()
-            );
+            this.fieldArray = this.fieldArray.filter((item: any) =>item.value.toLowerCase() !== field.value.toLowerCase());
           }
         }
       );
       this.disableDependentValuesForPreviousQuestion();
-    } else if (
-      this.currentPage === 3 ||
+    }else if( this.currentPage === 3){
+      let data =['AI (All Modules)','Analytics and BI (All Modules)','Content Management (All Modules)','CRM (All Modules)','Collaboration (All Modules)',
+      'eCommerce (All Modules)','ERP Financial (All Modules)','HCM (All Modules)','IoT Platform (All Modules)',
+      'ITSM (All Modules)','IaaS (All Modules)','PPM (All Modules)','PaaS (All Modules)','SCM (All Modules)','TRM (All Modules)'];
+      let PLM =['PLM (All Modules)'];
+      const plmData =['Bill of Material (BOM) Mgt.','Product Data Management (PDM)','Product Engineering','Document Management',
+      'Change Management','Design Management','Reciepe Management','Program Planning & Project Mgt.','Requirements Management',
+      'Platform Capabilities','Technical Documentation','Reporting','Service Management','Integration & Extensibility',
+      'Manufacturing Process Planning (MPP)','Digital Twin core'
+    ]
+      this.talentQuestionData[this.currentPage].option.forEach((element: any) => {
+       if(data.includes(field.value)){
+          element.disable = false;
+        }
+       else if(PLM.includes(field.value)){
+        const index = plmData.indexOf(element.value);
+        if(index !=-1){
+          element.disable = false;
+        }
+      }
+      else if(element.value.toLowerCase() === field.value.toLowerCase()){
+        element.disable = false;
+      }
+      if (element.value.toLowerCase() === field.value.toLowerCase()) {
+        element.selected = false;
+        element.disable = false;
+        element.noOfYear = element.skill = '';
+        this.fieldArray = this.fieldArray.filter((item: any) => item.value.toLowerCase() !== field.value.toLowerCase());
+      }  
+
+      }
+    );
+      this.disableNextQuesDependentValue();
+    }
+     else if (
       this.currentPage === 4 ||
       this.currentPage === 5 ||
       this.currentPage === 6 ||
       this.currentPage === 8 ||
       this.currentPage === 9
     ) {
-      this.talentQuestionData[this.currentPage].option.forEach(
-        (element: any) => {
+      this.talentQuestionData[this.currentPage].option.forEach((element: any) => {
           if (element.value.toLowerCase() === field.value.toLowerCase()) {
             element.selected = false;
             element.disable = false;
             if (this.currentPage != 9) {
               element.noOfYear = element.skill = '';
             }
-            this.fieldArray = this.fieldArray.filter(
-              (item: any) =>
-                item.value.toLowerCase() !== field.value.toLowerCase()
-            );
+            this.fieldArray = this.fieldArray.filter((item: any) => item.value.toLowerCase() !== field.value.toLowerCase());
             if (this.currentPage === 6) {
-              this.jobRole = this.jobRole.filter(
-                (item: any) =>
-                  item.value.toLowerCase() !== field.value.toLowerCase()
-              );
+              this.jobRole = this.jobRole.filter((item: any) => item.value.toLowerCase() !== field.value.toLowerCase());
             }
           }
 
-          if (
-            this.currentPage === 6 &&
-            this.getCountForSixQuestion() <= 3 &&
-            element.selected === false
-          ) {
+          if (this.currentPage === 6 && this.getCountForSixQuestion() <= 3 && element.selected === false) {
             element.disable = false;
           }
         }
       );
-      if (this.currentPage === 3) {
-        this.disableNextQuesDependentValue();
-      }
     }
   }
   disableDependentValuesForPreviousQuestion() {
@@ -922,6 +898,37 @@ export class TalentQuestionComponent implements OnInit {
     });
   }
 
+  getDisableForQue3(opt:any){
+    let data =['AI (All Modules)','Analytics and BI (All Modules)','Content Management (All Modules)','CRM (All Modules)','Collaboration (All Modules)',
+    'eCommerce (All Modules)','ERP Financial (All Modules)','HCM (All Modules)','IoT Platform (All Modules)',
+    'ITSM (All Modules)','IaaS (All Modules)','PPM (All Modules)','PaaS (All Modules)','SCM (All Modules)','TRM (All Modules)'];
+    let PLM =['PLM (All Modules)'];
+    const plmData =['Bill of Material (BOM) Mgt.','Product Data Management (PDM)','Product Engineering','Document Management',
+    'Change Management','Design Management','Reciepe Management','Program Planning & Project Mgt.','Requirements Management',
+    'Platform Capabilities','Technical Documentation','Reporting','Service Management','Integration & Extensibility',
+    'Manufacturing Process Planning (MPP)','Digital Twin core'
+  ]
+
+    if(data.includes(opt.value)){
+      opt.selected =true;
+      this.disableOtherValues(opt);
+    }
+    else if(PLM.includes(opt.value)){
+     opt.selected =true;
+     opt.disable =true;
+  this.talentQuestionData[this.currentPage].option.forEach((element: any) => {
+  if(plmData.includes(element.value)){
+    element.disable = true;
+  }
+    }
+  );
+    }
+    else{
+      opt.selected = true;
+      opt.disable = true;
+    }
+
+  }
   onlyNumber(evt: any) {
     evt = evt ? evt : window.event;
     var charCode = evt.which ? evt.which : evt.keyCode;
