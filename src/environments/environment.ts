f6e29b1f3df6294016b21b0aaa9113ee999c13df
Sameer Shukla <42176715+skv10@users.noreply.github.com>
2022-10-08 08:37:03 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://eas-works.herokuapp.com/api',
   imageUrl: 'https://eas-works.herokuapp.com/',
-  keyForCurrentlOCATION:'AIzaSyCYk0FaVxMT7okvvxur0bMLWaUmee_8Pqw'
 };
 
 /*
