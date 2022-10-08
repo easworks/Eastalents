f2dd6f06f57ac4f22d7b8e81dbc9102c4e4347f6
Sameer Shukla <42176715+skv10@users.noreply.github.com>
2022-10-08 08:37:40 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://eas-works.herokuapp.com/api',
-  imageUrl: 'https://eas-works.herokuapp.com/',
-  keyForCurrentlOCATION:'AIzaSyCYk0FaVxMT7okvvxur0bMLWaUmee_8Pqw'
+  imageUrl: 'https://eas-works.herokuapp.com/'
 };
