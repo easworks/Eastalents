e047216245da606828d02464e4cffdc90e0789ae
easworks <94338879+easworks@users.noreply.github.com>
2023-03-24 09:02:26 +0530

    changed environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://eas-works.herokuapp.com/api',
-  imageUrl: 'https://eas-works.herokuapp.com/'
+  apiUrl: 'https://eas-works.onrender.com/api',
+  imageUrl: 'https://eas-works.onrender.com/'
 };
