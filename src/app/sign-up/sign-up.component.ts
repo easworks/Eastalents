16b10398f30cc92491755cef99cefde85cb38807
ramkrishnaindal <ramkrishnaindalkar@gmail.com>
2023-05-24 10:57:15 +0530

    Urls made dynamic for signup and login based on environment variables

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -6,81 +6,95 @@ import { MustMatch } from '../_helpers/must-match.validator';
 import { ApiResponse, SignUpModel } from '../_models';
 import { HttpService } from '../_services/http.service';
 import { ToasterService } from '../_services/toaster.service';
-
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class SignUpComponent implements OnInit {
-
+  apiUrl = `${environment.apiUrl}`;
   submitted = false;
   signUpForm!: FormGroup;
-  emailForRoute='';
-  firstName='';
-  lastName='';
-  userDataFromRoute:any;
-  githubLogin:boolean = false;
+  emailForRoute = '';
+  firstName = '';
+  lastName = '';
+  userDataFromRoute: any;
+  githubLogin: boolean = false;
 
-  constructor(private formBuilder: FormBuilder, private toaster: ToasterService,
-              private httpService: HttpService, private router: Router,private route: ActivatedRoute) {
-                this.route.queryParams.subscribe((res)=>{
-                  if(res && res.user){
-                  this.userDataFromRoute = JSON.parse(res.user);
-                  this.emailForRoute = this.userDataFromRoute.email || '';
-                  this.firstName = this.userDataFromRoute.firstName || '';
-                  this.lastName = this.userDataFromRoute.lastName || '';
-                  }
-                 })
-   }
+  constructor(
+    private formBuilder: FormBuilder,
+    private toaster: ToasterService,
+    private httpService: HttpService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParams.subscribe((res) => {
+      if (res && res.user) {
+        this.userDataFromRoute = JSON.parse(res.user);
+        this.emailForRoute = this.userDataFromRoute.email || '';
+        this.firstName = this.userDataFromRoute.firstName || '';
+        this.lastName = this.userDataFromRoute.lastName || '';
+      }
+    });
+  }
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     this.initSignUpForm();
   }
 
   initSignUpForm(): void {
-    this.signUpForm = this.formBuilder.group({
-      firstName: [this.firstName, Validators.required],
-      lastName: [this.lastName, Validators.required],
-      email: [this.emailForRoute, [Validators.required, Validators.pattern(pattern.email)]],
-      password: ['', [Validators.required, Validators.pattern(pattern.password)]],
-      confirmPassword: ['', Validators.required],
-      acceptTerms: [false, Validators.requiredTrue],
-      userRole: [roles.talent]
-    }, {
-      validator: MustMatch('password', 'confirmPassword')
-    });
+    this.signUpForm = this.formBuilder.group(
+      {
+        firstName: [this.firstName, Validators.required],
+        lastName: [this.lastName, Validators.required],
+        email: [
+          this.emailForRoute,
+          [Validators.required, Validators.pattern(pattern.email)],
+        ],
+        password: [
+          '',
+          [Validators.required, Validators.pattern(pattern.password)],
+        ],
+        confirmPassword: ['', Validators.required],
+        acceptTerms: [false, Validators.requiredTrue],
+        userRole: [roles.talent],
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword'),
+      }
+    );
   }
 
- 
-
   // tslint:disable-next-line:typedef
-  get signUpFormControl() { return this.signUpForm.controls; }
+  get signUpFormControl() {
+    return this.signUpForm.controls;
+  }
 
   getStarted(): void {
     this.submitted = true;
     if (this.signUpForm.invalid) {
       return;
     }
-    this.httpService.post(`users/signup`, this.signUpForm.value).subscribe((response: ApiResponse<any>) => {
-      if (response.status) {
-        this.router.navigate(['/talentprofilequestion']);
-        this.toaster.success(`${response.message}`);
-        this.signUpForm.reset();
-        this.submitted = false;
-      } else {
-        this.toaster.error(`${response.message}`);
-      }
-    });
+    this.httpService
+      .post(`users/signup`, this.signUpForm.value)
+      .subscribe((response: ApiResponse<any>) => {
+        if (response.status) {
+          this.router.navigate(['/talentprofilequestion']);
+          this.toaster.success(`${response.message}`);
+          this.signUpForm.reset();
+          this.submitted = false;
+        } else {
+          this.toaster.error(`${response.message}`);
+        }
+      });
   }
 
-
-  showGithubLogin(){
+  showGithubLogin() {
     this.githubLogin = true;
   }
 
-  backToGmailLogin(){
+  backToGmailLogin() {
     this.githubLogin = false;
-   }
-
+  }
 }
