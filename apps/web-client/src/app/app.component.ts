36a0714d561ab57babf77ef8d7a283c3d2bd0e4c
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-01 03:26:22 +0530

    update

--- apps/web-client/src/app/app.component.ts
+++ apps/web-client/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
