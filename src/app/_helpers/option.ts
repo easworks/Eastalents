3954943684bb362e611336d1a6067078edf52513
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-01 02:12:55 +0530

    remove old code completely

--- src/app/_helpers/option.ts
+++
@@ -1,9 +0,0 @@
-export interface Option<T = string> {
-  value: T;
-  label?: string;
-  title?: string;
-}
-
-export interface SelectableOption<T = string> extends Option<T> {
-  selected: boolean;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://eas-works.onrender.com/api',
-  imageUrl: 'https://eas-works.onrender.com/',
-  keyForCurrentlOCATION: '',
-};
