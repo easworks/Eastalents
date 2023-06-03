09e5c706ff08e51bcf6b12e21f2884b85fb79f3d
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-03 06:14:04 +0530

    create api service

---
+++ libs/app-shell/src/services/api.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  handleError(error: any) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    // window.alert(errorMessage);
+    return throwError(() => new Error(errorMessage));
+  }
+}
