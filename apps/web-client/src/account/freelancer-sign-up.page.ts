fa8868e1a17ebdf4346d3e3faf16248b19087668
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-04 05:04:28 +0530

    create the freelancer sign up page layout

--- apps/web-client/src/account/freelancer-sign-up.page.ts
+++ apps/web-client/src/account/freelancer-sign-up.page.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { LottiePlayerDirective } from '@easworks/app-shell';
 
 @Component({
   selector: 'account-freelancer-sign-up-page',
   templateUrl: './freelancer-sign-up.page.html',
+  styleUrls: ['./freelancer-sign-up.page.less'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    LottiePlayerDirective
+  ]
 })
 export class FreelancerSignUpPageComponent {
   @HostBinding()
-  private readonly class = 'page';
+  private readonly class = 'page grid lg:flex gap-4 justify-center';
 }
