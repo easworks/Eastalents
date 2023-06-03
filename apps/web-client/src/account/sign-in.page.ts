49f02b63e7381bb46e4970370c9370fd8d578996
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-04 01:53:23 +0530

    update sign in page

--- apps/web-client/src/account/sign-in.page.ts
+++ apps/web-client/src/account/sign-in.page.ts
@@ -1,12 +1,17 @@
 import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ImportsModule } from '@easworks/app-shell';
 
 @Component({
   selector: 'account-sign-in-page',
   templateUrl: './sign-in.page.html',
+  styleUrls: ['./sign-in.page.less'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ImportsModule
+  ]
 })
 export class AccountSignInPageComponent {
   @HostBinding()
-  private readonly class = 'page';
+  private readonly class = 'page grid place-content-center';
 }
