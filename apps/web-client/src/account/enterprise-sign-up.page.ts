e2d141cb1a8a40fc4df084448fd6edc66720ebeb
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-04 09:34:11 +0530

    update employer sign up page

--- apps/web-client/src/account/enterprise-sign-up.page.ts
+++ apps/web-client/src/account/enterprise-sign-up.page.ts
@@ -3,8 +3,10 @@ import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 @Component({
   selector: 'account-enterprise-sign-up-page',
   templateUrl: './enterprise-sign-up.page.html',
+  styleUrls: ['./enterprise-sign-up.page.less'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: []
 })
 export class EnterpriseSignUpPageComponent {
   @HostBinding()
