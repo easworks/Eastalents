ba4e551fb0119f06b511a1fc0e6f786d95311129
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-05 04:29:14 +0530

    move the routes

---
+++ apps/web-client/src/account/routes.ts
@@ -0,0 +1,47 @@
+import { Route } from '@angular/router';
+
+export const ACCOUNT_ROUTES: Route = {
+  path: 'account',
+  children: [
+    {
+      path: 'sign-up/freelancer',
+      pathMatch: 'full',
+      loadComponent: () => import('./freelancer-sign-up.page').then(m => m.FreelancerSignUpPageComponent),
+      data: {
+        seo: {
+          title: () => 'Sign Up as Freelancer'
+        }
+      }
+    },
+    {
+      path: 'sign-up/enterprise',
+      pathMatch: 'full',
+      loadComponent: () => import('./enterprise-sign-up.page').then(m => m.EnterpriseSignUpPageComponent),
+      data: {
+        seo: {
+          title: () => 'Sign Up as Enterprise'
+        }
+      }
+    },
+    {
+      path: 'sign-in',
+      pathMatch: 'full',
+      loadComponent: () => import('./sign-in.page').then(m => m.AccountSignInPageComponent),
+      data: {
+        seo: {
+          title: () => 'Sign In'
+        }
+      }
+    },
+    {
+      path: 'reset-password',
+      pathMatch: 'full',
+      loadComponent: () => import('./password-reset.page').then(m => m.AccountPasswordResetPageComponent),
+      data: {
+        seo: {
+          title: () => 'Reset Password'
+        }
+      }
+    }
+  ]
+};
--- apps/web-client/src/app/routes.ts
+++ apps/web-client/src/app/routes.ts
@@ -1,53 +1,10 @@
 import { Routes } from '@angular/router';
 import { NotFoundPageComponent } from '@easworks/app-shell';
+import { ACCOUNT_ROUTES } from '../account/routes';
 
 
 export const routes: Routes = [
-  {
-    path: 'account',
-    children: [
-      {
-        path: 'sign-up/freelancer',
-        pathMatch: 'full',
-        loadComponent: () => import('../account/freelancer-sign-up.page').then(m => m.FreelancerSignUpPageComponent),
-        data: {
-          seo: {
-            title: () => 'Sign Up as Freelancer'
-          }
-        }
-      },
-      {
-        path: 'sign-up/enterprise',
-        pathMatch: 'full',
-        loadComponent: () => import('../account/enterprise-sign-up.page').then(m => m.EnterpriseSignUpPageComponent),
-        data: {
-          seo: {
-            title: () => 'Sign Up as Enterprise'
-          }
-        }
-      },
-      {
-        path: 'sign-in',
-        pathMatch: 'full',
-        loadComponent: () => import('../account/sign-in.page').then(m => m.AccountSignInPageComponent),
-        data: {
-          seo: {
-            title: () => 'Sign In'
-          }
-        }
-      },
-      {
-        path: 'reset-password',
-        pathMatch: 'full',
-        loadComponent: () => import('../account/password-reset.page').then(m => m.AccountPasswordResetPageComponent),
-        data: {
-          seo: {
-            title: () => 'Reset Password'
-          }
-        }
-      }
-    ]
-  },
+  ACCOUNT_ROUTES,
   {
     path: '**',
     loadComponent: () => NotFoundPageComponent
