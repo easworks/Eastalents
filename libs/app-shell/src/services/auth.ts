b9bb7c8ddd1afffad1927e6277d24cec174dbfa3
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-05 22:26:30 +0530

    Update auth.ts

--- libs/app-shell/src/services/auth.ts
+++ libs/app-shell/src/services/auth.ts
@@ -1,5 +1,5 @@
 import { Injectable, inject } from '@angular/core';
-import { AuthNotFound, GoogleCallbackState, SocialCallbackState } from '@easworks/models';
+import { AuthNotFound, GoogleCallbackState, SocialCallbackState, SocialIdp } from '@easworks/models';
 import { AccountApi } from '../api';
 
 
@@ -22,7 +22,7 @@ export class AuthService {
     },
     google: (returnUrl?: string) => {
       console.debug('[SIGN IN] Google');
-      const authUrl = constructGoogleAuthUrl();
+      const authUrl = AuthRedirect.getUrl('google');
       const state = JSON.stringify({
         provider: 'google',
         returnUrl
@@ -46,13 +46,47 @@ export class AuthService {
 }
 
 
+interface AuthRedirectConfig {
+  readonly url: string;
+  readonly response_type: string;
+  readonly redirect_uri: string;
+  readonly client_id: string;
+  readonly scope?: string;
+}
 
+class AuthRedirect {
+  private static readonly configs: Readonly<Record<SocialIdp, AuthRedirectConfig>> = {
+    google: {
+      url: 'https://accounts.google.com/o/oauth2/v2/auth',
+      response_type: 'code',
+      redirect_uri: `${window.location.origin}/account/social/callback`,
+      client_id: '375730135906-ue24tu42t280a93645tb9r68sfe03jme.apps.googleusercontent.com',
+      scope: 'email profile'
+    },
+    linkedin: {
+      url: 'https://www.linkedin.com/oauth/v2/authorization',
+      response_type: 'code',
+      redirect_uri: `${window.location.origin}/account/social/callback`,
+      client_id: '375730135906-ue24tu42t280a93645tb9r68sfe03jme.apps.googleusercontent.com',
+      scope: 'email profile'
+    },
+    github: {
+      url: 'https://github.com/login/oauth/authorize',
+      response_type: 'code',
+      redirect_uri: `${window.location.origin}/account/social/callback`,
+      client_id: '375730135906-ue24tu42t280a93645tb9r68sfe03jme.apps.googleusercontent.com',
+      scope: 'user:read'
+    }
+  };
 
-function constructGoogleAuthUrl() {
-  const authUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
-  authUrl.searchParams.set('response_type', 'code');
-  authUrl.searchParams.set('redirect_uri', `https://eas-works.onrender.com/api/gmail/callback`);
-  authUrl.searchParams.set('client_id', '375730135906-ue24tu42t280a93645tb9r68sfe03jme.apps.googleusercontent.com');
-  authUrl.searchParams.set('scope', 'email profile');
-  return authUrl;
+  static getUrl(provider: SocialIdp) {
+    const config = this.configs[provider];
+    const authUrl = new URL(config.url);
+    authUrl.searchParams.set('response_type', config.response_type);
+    authUrl.searchParams.set('redirect_uri', config.redirect_uri);
+    authUrl.searchParams.set('client_id', config.client_id);
+    if (config.scope)
+      authUrl.searchParams.set('scope', config.scope);
+    return authUrl;
+  }
 }
