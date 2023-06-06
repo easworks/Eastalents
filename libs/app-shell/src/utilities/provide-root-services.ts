d2c9dfbccf446ccc0713a5a0f8034907b345d961
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-06 07:45:11 +0530

    Update provide-root-services.ts

--- libs/app-shell/src/utilities/provide-root-services.ts
+++ libs/app-shell/src/utilities/provide-root-services.ts
@@ -1,5 +1,6 @@
 import { APP_INITIALIZER, Provider } from '@angular/core';
 
+/** Forces the creation of service instances even when it is not injected in any component */
 export function provideRootServices(...providers: Provider[]): Provider {
   return {
     provide: APP_INITIALIZER,
