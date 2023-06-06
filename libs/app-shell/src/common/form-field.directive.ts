69fdb30a1d8a25d3b9e01bf7ab2c7ebe40402fc0
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-07 03:12:34 +0530

    simplify form-field directive

--- libs/app-shell/src/common/form-field.directive.ts
+++ libs/app-shell/src/common/form-field.directive.ts
@@ -1,15 +1,11 @@
-import { ChangeDetectorRef, DestroyRef, Directive, HostBinding, Input, OnInit, inject } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Directive, HostBinding, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
 @Directive({
   standalone: true,
   selector: '[formFieldControl]',
 })
-export class FormFieldDirective implements OnInit {
-  private readonly dRef = inject(DestroyRef);
-  private readonly cdRef = inject(ChangeDetectorRef);
-
+export class FormFieldDirective {
   @Input({ required: true, alias: 'formFieldControl' }) public control!: FormControl;
 
   @HostBinding() private get class() {
@@ -19,13 +15,4 @@ export class FormFieldDirective implements OnInit {
       this.control.dirty ? 'dirty' : undefined
     ];
   }
-
-  ngOnInit(): void {
-    this.control.statusChanges
-      .pipe(takeUntilDestroyed(this.dRef))
-      .subscribe(() => {
-        this.cdRef.markForCheck()
-      })
-  }
-
 }
