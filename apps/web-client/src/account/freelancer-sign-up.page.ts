c02005bbbff22c4286da0dbd4a49dccd8248549f
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-06 14:31:28 +0530

    add basic signup handler for freelancers

--- apps/web-client/src/account/freelancer-sign-up.page.ts
+++ apps/web-client/src/account/freelancer-sign-up.page.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, HostBinding, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { AuthService, FormImports, ImportsModule, LottiePlayerDirective } from '@easworks/app-shell';
-import { pattern } from '@easworks/models';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AccountApi, AuthService, ErrorSnackbarDefaults, FormImports, ImportsModule, LottiePlayerDirective, SnackbarComponent, generateLoadingState } from '@easworks/app-shell';
+import { SignUpRequest, pattern } from '@easworks/models';
 
 @Component({
   selector: 'account-freelancer-sign-up-page',
@@ -19,9 +20,15 @@ import { pattern } from '@easworks/models';
 })
 export class FreelancerSignUpPageComponent {
   protected readonly auth = inject(AuthService);
+  protected readonly api = {
+    account: inject(AccountApi)
+  } as const;
+  private readonly snackbar = inject(MatSnackBar);
 
   @HostBinding()
   private readonly class = 'page grid grid-cols-10 gap-4';
+  protected readonly loading = generateLoadingState<['signing up']>();
+
 
   protected readonly form = new FormGroup({
     firstName: new FormControl('', {
@@ -60,4 +67,34 @@ export class FreelancerSignUpPageComponent {
     ],
     updateOn: 'submit'
   })
+
+  submit() {
+    if (!this.form.valid)
+      return;
+
+    const { email, firstName, lastName, password } = this.form.getRawValue();
+
+    const input: SignUpRequest = {
+      provider: null,
+      email,
+      firstName,
+      lastName,
+      password,
+      role: 'freelancer'
+    };
+
+    this.loading.add('signing up');
+    this.api.account.signUp(input)
+      .subscribe({
+        next: () => {
+          this.loading.delete('signing up')
+        },
+        error: () => {
+          this.snackbar.openFromComponent(SnackbarComponent, ErrorSnackbarDefaults);
+          this.loading.delete('signing up');
+        }
+      })
+
+    this.loading.delete('signing up');
+  }
 }
