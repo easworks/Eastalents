29581c02e4d775c1051be9f264d0da11a4acfbfa
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-11 06:33:27 +0530

    create the welcome banner

--- apps/web-client/src/freelancer/profile-edit.page.ts
+++ apps/web-client/src/freelancer/profile-edit.page.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ImportsModule, LottiePlayerDirective } from '@easworks/app-shell';
 
 @Component({
   selector: 'freelancer-profile-edit-page',
   templateUrl: './profile-edit.page.html',
   styleUrls: ['./profile-edit.page.less'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ImportsModule,
+    LottiePlayerDirective
+  ]
 })
 export class FreelancerProfileEditPageComponent {
 
+  private readonly route = inject(ActivatedRoute);
+
+  @HostBinding() private readonly class = 'flex flex-col lg:flex-row';
+
+  protected readonly isNew = this.route.snapshot.queryParamMap.has('new');
 }
