fef4c45e735b4ede2dbdd62a692349c08942f667
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-12 18:01:13 +0530

    sort the country list

--- libs/app-shell/src/api/location.ts
+++ libs/app-shell/src/api/location.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { ApiService } from './api';
 import { ApiResponse, Country, Province } from '@easworks/models';
-import { catchError, delay, map } from 'rxjs';
+import { catchError, map } from 'rxjs';
+import { sortString } from '../utilities';
+import { ApiService } from './api';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,11 @@ export class LocationApi extends ApiService {
   readonly countries = () =>
     this.http.get<ApiResponse>(`${this.apiUrl}/location/getCountries`)
       .pipe(
-        map(r => r['countries'] as Country[]),
+        map(r => {
+          const countries = r['countries'] as Country[];
+          countries.sort((a, b) => sortString(a.name, b.name));
+          return countries;
+        }),
         catchError(this.handleError)
       );
 
