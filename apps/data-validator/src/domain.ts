554b33ea25d309bac9b6ca80f273234f76a59736
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-06-27 01:52:14 +0530

    add a saved domain response

--- apps/data-validator/src/domain.ts
+++ apps/data-validator/src/domain.ts
@@ -1,16 +1,17 @@
 import { sortString } from '@easworks/app-shell/utilities/sort';
+import { Software } from '@easworks/models';
 import { writeFile } from 'fs/promises';
 
-function getDomains() {
-  return fetch('http://eas-works.onrender.com/api/talentProfile/getTalentProfileSteps')
-    .then(r => r.json())
-    .then(m => m.talentProfile);
+export function getDomains() {
+  // return fetch('http://eas-works.onrender.com/api/talentProfile/getTalentProfileSteps')
+  //   .then(r => r.json())
+  //   .then(m => m.talentProfile);
 
-  // return import(new URL('./assets/domain-response.json', import.meta.url).toString(), { assert: { type: 'json' } })
-  // .then(m => m.default.talentProfile);
+  return import(new URL('./domain-response.json', import.meta.url).toString(), { assert: { type: 'json' } })
+    .then(m => m.default.talentProfile);
 }
 
-async function processDomains(domains: any) {
+export async function extractData(domains: any) {
 
   const todo: Promise<void>[] = [];
 
@@ -22,7 +23,7 @@ async function processDomains(domains: any) {
         .forEach((p: any) => softSet.add(p.name))));
 
   {
-    const record: Record<string, any[]> = {};
+    const record: Record<string, string[]> = {};
     [...softSet.values()].sort(sortString).forEach(v => record[v] = []);
 
     todo.push(writeFile('./libs/shared/assets/src/utils/software.json', JSON.stringify(record, null, 2) + '\n', { encoding: 'utf-8' }));
@@ -31,7 +32,13 @@ async function processDomains(domains: any) {
   await Promise.all(todo);
 }
 
-export async function fetchAndProcessDomains() {
-  const domains = await getDomains();
-  await processDomains(domains);
+export function validateData(domains: any, software: Map<string, Software>) {
+  Object.keys(domains)
+    .forEach(domain => Object.keys(domains[domain].Modules)
+      .forEach(module => domains[domain].Modules[module].Product
+        .forEach((p: any) => {
+          if (!software.has(p.name)) {
+            console.error(`software: '${domain}/${module}/${p.name}' was not found`)
+          }
+        })))
 }
--- apps/data-validator/src/main.ts
+++ apps/data-validator/src/main.ts
@@ -1,26 +1,26 @@
 import { parse } from '@easworks/app-shell/state/parsers';
+import { getDomains, validateData } from './domain';
 
 async function main() {
-
-  // fetchAndProcessDomains();
-
-  console.log('reading tech.json');
-  const techInput = await import(new URL('./tech.json', import.meta.url).toString(), { assert: { type: 'json' } })
+  console.log('reading input');
+  const techInput = import(new URL('./tech.json', import.meta.url).toString(), { assert: { type: 'json' } })
     .then(m => m.default);
+  const softwareInput = import(new URL('./software.json', import.meta.url).toString(), { assert: { type: 'json' } })
+    .then(m => m.default);
+  const domains = getDomains();
+
+
 
   console.log('validating tech.json');
-  const techOutput = parse.techDto(techInput, true);
-
-
-  console.log('reading software.json');
-  const softwareInput = await import(new URL('./software.json', import.meta.url).toString(), { assert: { type: 'json' } })
-    .then(m => m.default);
+  const techOutput = parse.techDto(await techInput, true);
 
   console.log('validating software.json');
-  const softwareOutput = parse.softwareDto(softwareInput, true, techOutput);
+  const softwareOutput = parse.softwareDto(await softwareInput, true, techOutput);
 
-  console.debug(softwareOutput);
+  console.log('validating domains');
+  validateData(await domains, softwareOutput);
 
+  console.log('finished');
 };
 
 await main();
