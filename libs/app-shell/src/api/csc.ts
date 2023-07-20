547c1d72b7f109eacebe411c1b93b8286942b0ed
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-07-20 10:24:35 +0530

    Update csc.ts

--- libs/app-shell/src/api/csc.ts
+++ libs/app-shell/src/api/csc.ts
@@ -1,6 +1,9 @@
 import { Injectable, inject, isDevMode } from '@angular/core';
 import { ENVIRONMENT } from '../environment';
 import { Cached, createCache, isFresh } from './cache';
+import { generateLoadingState } from '../state/loading';
+
+const THREE_DAY_MS = 3 * 24 * 60 * 60 * 1000;
 
 @Injectable({
   providedIn: 'root'
@@ -15,41 +18,151 @@ export class CSCApi {
   private readonly devMode = isDevMode();
   private readonly cache = createCache('csc-cache');
 
-  async getAllCountries() {
-    const url = `${this.apiUrl}/countries`;
-    const id = url;
-    const cached = await this.cache.get<Cached<Country[]>>(id);
+  readonly loading = generateLoadingState<[
+    'country',
+    'state',
+    'city'
+  ]>();
 
-    if (cached && isFresh(cached))
-      return cached.data;
+  async allCountries() {
+    this.loading.add('country');
 
-    const res = await fetch(url, { headers: this.headers });
-    await this.handleErrorsIfAny(res);
+    try {
+      const url = `${this.apiUrl}/countries`;
+      const id = url;
 
-    const countries = await res.json() as Country[];
+      const cached = await this.cache.get<Cached<Country[]>>(id);
+      if (cached && isFresh(cached, THREE_DAY_MS))
+        return cached.data;
 
-    const details = await Promise.all(countries.map(c => this.countryDetails(c.iso2)))
+      const res = await fetch(url, { headers: this.headers });
+      await this.handleErrorsIfAny(res);
 
-    await this.cache.set(id, details);
-    return details;
+      const countries = await res.json() as Country[];
+
+      const details = await Promise.all(countries.map(c => this.countryDetails(c.iso2)))
+
+      await this.cache.set(id, details);
+      return details;
+    }
+    finally {
+      this.loading.delete('country');
+    }
   }
 
-  async countryDetails(iso2: string) {
-    const url = `${this.apiUrl}/countries/${iso2}`;
-    const id = url;
+  async allStates(ciso2: string) {
+    this.loading.add('state');
 
-    const cached = await this.cache.get<Cached<Country>>(id);
-    if (cached && isFresh(cached))
-      return cached.data;
+    try {
+      const url = `${this.apiUrl}/countries/${ciso2}/states`;
+      const id = url;
 
-    const res = await fetch(url, { headers: this.headers });
-    await this.handleErrorsIfAny(res);
+      const cached = await this.cache.get<Cached<State[]>>(id);
+      if (cached && isFresh(cached, THREE_DAY_MS))
+        return cached.data;
 
-    const country = await res.json() as Country;
-    country.timezones = JSON.parse(country.timezones as unknown as string);
-    country.translations = JSON.parse(country.translations as unknown as string);
-    await this.cache.set(id, country);
-    return country;
+      const res = await fetch(url, { headers: this.headers });
+      await this.handleErrorsIfAny(res);
+
+      const states = await res.json() as State[];
+
+      const details = await Promise.all(states.map(s => this.stateDetails(ciso2, s.iso2)));
+
+      await this.cache.set(id, details);
+      return details;
+    }
+    finally {
+      this.loading.delete('state');
+    }
+  }
+
+  async allCities(ciso2: string, siso2?: string) {
+    const isChildOperation = this.loading.has('city')();
+    if (!isChildOperation)
+      this.loading.add('city');
+
+    try {
+      const url = siso2 ?
+        `${this.apiUrl}/countries/${ciso2}/states/${siso2}/cities` :
+        `${this.apiUrl}/countries/${ciso2}/cities`;
+      const id = url;
+
+      const cached = await this.cache.get<Cached<City[]>>(id);
+      if (cached && isFresh(cached, THREE_DAY_MS))
+        return cached.data;
+
+      const res = await fetch(url, { headers: this.headers });
+      await this.handleErrorsIfAny(res);
+
+      const cities = await res.json() as City[];
+      await this.cache.set(id, cities);
+      return cities;
+    }
+    finally {
+      if (!isChildOperation)
+        this.loading.delete('city');
+    }
+  }
+
+  async countryDetails(ciso2: string) {
+    const isChildOperation = this.loading.has('country')();
+    if (!isChildOperation)
+      this.loading.add('country');
+
+    try {
+      const url = `${this.apiUrl}/countries/${ciso2}`;
+      const id = url;
+
+      const cached = await this.cache.get<Cached<Country>>(id);
+      if (cached && isFresh(cached, THREE_DAY_MS))
+        return cached.data;
+
+      const res = await fetch(url, { headers: this.headers });
+      await this.handleErrorsIfAny(res);
+
+      const country = await res.json() as Country;
+      country.timezones = JSON.parse(country.timezones as unknown as string);
+      country.translations = JSON.parse(country.translations as unknown as string);
+      await this.cache.set(id, country);
+      return country;
+    }
+    finally {
+      if (!isChildOperation)
+        this.loading.delete('country');
+    }
+
+  }
+
+  async stateDetails(ciso2: string, siso2: string) {
+    const isChildOperation = this.loading.has('state')();
+    if (!isChildOperation)
+      this.loading.add('state');
+
+    try {
+      if (siso2.length !== 2) {
+        const parts = siso2.split('-');
+        ciso2 = parts[0];
+        siso2 = parts[1];
+      }
+
+      const url = `${this.apiUrl}/countries/${ciso2}/states/${siso2}`;
+      const id = url;
+
+      const cached = await this.cache.get<Cached<State>>(id);
+      if (cached && isFresh(cached, THREE_DAY_MS))
+        return cached.data;
+
+      const res = await fetch(url, { headers: this.headers });
+      await this.handleErrorsIfAny(res);
+
+      const state = await res.json() as State;
+      await this.cache.set(id, state);
+      return state;
+    }
+    finally {
+      if (!isChildOperation)
+        this.loading.delete('state');
+    }
   }
 
   private async handleErrorsIfAny(response: Response) {
@@ -69,11 +182,26 @@ export interface Country {
   name: string;
   iso2: string;
   iso3: string;
+
   currency: string;
   phonecode: string;
   emoji: string;
   timezones: Timezone[];
   translations: { [key: string]: string };
+}
+
+export interface State {
+  id: number;
+  name: string;
+  iso2: string;
+
+  country_code: string;
+  country_id: number;
+}
+
+export interface City {
+  id: number;
+  name: string;
 }
 
 interface Timezone {
