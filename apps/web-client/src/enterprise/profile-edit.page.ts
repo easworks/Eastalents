156bf8f1ea4d9b168da3efc5b1dd07ae0d11554e
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-07-30 19:29:18 +0530

    add the side banner for the profile

--- apps/web-client/src/enterprise/profile-edit.page.ts
+++ apps/web-client/src/enterprise/profile-edit.page.ts
@@ -1,10 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ImportsModule } from '@easworks/app-shell/common/imports.module';
+import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
 
 @Component({
   selector: 'enterprise-profile-edit-page',
   templateUrl: './profile-edit.page.html',
   styleUrls: ['./profile-edit.page.less'],
   standalone: true,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ImportsModule,
+    LottiePlayerDirective
+  ]
 })
-export class EnterpriseProfileEditPageComponent { }
+export class EnterpriseProfileEditPageComponent {
+  private readonly route = inject(ActivatedRoute);
+  @HostBinding() private readonly class = 'flex flex-col lg:flex-row';
+
+  protected readonly isNew = this.route.snapshot.queryParamMap.has('new');
+}
