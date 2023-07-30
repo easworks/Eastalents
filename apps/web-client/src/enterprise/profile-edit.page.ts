1719d5ed30ed30e21d57af443a474fbd300faf47
Dibyodyuti Mondal <50345677+DibyodyutiMondal@users.noreply.github.com>
2023-07-30 19:45:19 +0530

    update controls

--- apps/web-client/src/enterprise/profile-edit.page.ts
+++ apps/web-client/src/enterprise/profile-edit.page.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, HostBinding, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { FormImportsModule } from '@easworks/app-shell/common/form.imports.module';
 import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
 
@@ -11,7 +13,8 @@ import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     ImportsModule,
-    LottiePlayerDirective
+    LottiePlayerDirective,
+    FormImportsModule
   ]
 })
 export class EnterpriseProfileEditPageComponent {
@@ -19,4 +22,21 @@ export class EnterpriseProfileEditPageComponent {
   @HostBinding() private readonly class = 'flex flex-col lg:flex-row';
 
   protected readonly isNew = this.route.snapshot.queryParamMap.has('new');
+
+  protected readonly form = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(300)
+      ]
+    }),
+    summary: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.maxLength(1500)
+      ]
+    })
+  })
 }
