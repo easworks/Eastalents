0a138c2a42706246377503fe6252b4ffb373d213
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-08-07 05:14:18 +0530

    refactor employer sign up page

--- libs/app-shell/src/state/loading.ts
+++ libs/app-shell/src/state/loading.ts
@@ -1,4 +1,4 @@
-import { computed, signal } from '@angular/core';
+import { Signal, computed, effect, signal } from '@angular/core';
 
 export function generateLoadingState<T extends readonly string[]>() {
   type L = T[number];
@@ -6,17 +6,30 @@ export function generateLoadingState<T extends readonly string[]>() {
   const size$ = computed(() => set$().size);
   const any$ = computed(() => size$() > 0);
 
-  return {
+  const ops = {
     add: (value: L) => {
-      set$.mutate(s => s.add(value))
+      set$.mutate(s => s.add(value));
     },
     delete: (value: L) => {
-      set$.mutate(s => s.delete(value))
+      set$.mutate(s => s.delete(value));
     },
     clear: () => {
-      set$.mutate(s => s.clear())
-    },
+      set$.mutate(s => s.clear());
+    }
+  } as const;
+
+  return {
+    ...ops,
+
     has: (value: L) => computed(() => set$().has(value)),
+    react: (value: L, src: Signal<boolean>) => {
+      effect(() => {
+        if (src())
+          ops.add(value);
+        else
+          ops.delete(value);
+      }, { allowSignalWrites: true });
+    },
 
     set$,
     size$,
