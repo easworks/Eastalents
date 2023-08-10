f6a3b1364525bbc4e1ee1065c2bb5cc94a591243
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-08-10 07:27:57 +0530

    update

--- apps/web-client/src/public/home/home.page.ts
+++ apps/web-client/src/public/home/home.page.ts
@@ -67,6 +67,32 @@ export class HomePageComponent {
     },
   ];
 
+  protected readonly howItWorks = [
+    {
+      lottie: 'https://lottie.host/5bf7837c-438a-4a4e-a94d-7a36e8b9562a/cMaf4XaF50.json',
+      title: 'Describe your needs',
+      content: `Brief us on your Enterprise Application, technology, domain, 
+        enterprise project requirements and describe your project using our job form.`
+    },
+    {
+      lottie: 'https://lottie.host/3a6e85a5-efa3-4b74-8850-f8595245abd1/gfULmGWGsu.json',
+      title: 'Schedule a meeting to align on goals',
+      content: `We always create a unique team augmentation strategy based on business goals`
+    },
+    {
+      lottie: 'https://lottie.host/e53c1145-18b9-42c3-9dd5-30ba9473df9e/XmL6zR3gAz.json',
+      title: 'Interview & Approve',
+      content: `Interview the EAS talent that we match you with and approve the 
+        candidate(s) who will join your enterprise project team.`
+    },
+    {
+      lottie: 'https://lottie.host/f707add6-d1ed-49db-b0d2-224e018b4dca/4LiVNG5Um8.json',
+      title: 'Start your Enterprise project with remote talent',
+      content: `Seal the deal with a contract based on your engagement model and 
+        launch your remote team.`
+    }
+  ];
+
   protected readonly useCases = [
     {
       lottie: 'https://lottie.host/4c9690fd-2f7c-4f2d-8d17-189cd10faa13/ZVqeAIMqEv.json',
