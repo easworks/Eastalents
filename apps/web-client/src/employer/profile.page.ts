d33b46613fb277576a440940d001ce940fd6cda0
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-08-11 21:22:25 +0530

    update

--- apps/web-client/src/employer/profile.page.ts
+++ apps/web-client/src/employer/profile.page.ts
@@ -1,16 +1,21 @@
 import { ChangeDetectionStrategy, Component, HostBinding, inject, signal } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { EmployerApi } from '@easworks/app-shell/api/employer.api';
+import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { AuthState } from '@easworks/app-shell/state/auth';
 import { generateLoadingState } from '@easworks/app-shell/state/loading';
 import { EmployerProfile } from '@easworks/models';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   standalone: true,
   selector: 'employer-profile-page',
   templateUrl: './profile.page.html',
   styleUrls: ['./profile.page.less'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ImportsModule
+  ]
 })
 export class EmployerProfilePageComponent {
   constructor() {
@@ -24,6 +29,7 @@ export class EmployerProfilePageComponent {
   private readonly user$ = inject(AuthState).guaranteedUser();
   private readonly snackbar = inject(MatSnackBar);
 
+  protected readonly icons = { faUser } as const;
   @HostBinding() private readonly class = 'page';
 
   protected readonly loading = generateLoadingState<[
@@ -64,7 +70,21 @@ export class EmployerProfilePageComponent {
 const dummyData: EmployerProfile = {
   "_id": "646fa97b9f56f3d874be2ae3",
   "orgName": "some name",
-  "description": "some description",
+  "description": `Step into excellence with XYZ Corporation, an industry pacesetter renowned for innovation and impact. With a storied history of pioneering solutions on a global scale, our distinguished team drives us towards new horizons collaboratively.
+
+  At XYZ Corporation, innovation is embedded within our core. Our legacy includes breakthroughs spanning AI advancements to sustainable energy triumphs. As a vital part of our accomplished ensemble, you will be at the forefront of defining the future.
+  
+  Elevating careers is our forte. As part of our esteemed company, you'll experience unparalleled growth and advancement opportunities. Our array of resources, including mentorship initiatives, workshops, and access to premier conferences, propels your career journey.
+  
+  Our thriving culture is the cornerstone of our triumphs. Inclusivity, diversity, and empowerment are not mere buzzwords; they are integral to our triumphs. We offer a conducive work environment, where your unique insights continue to fuel our continued success.
+  
+  Balancing work and life is paramount. Our flexible policies prioritize your well-being, while our state-of-the-art facilities set the stage for your accomplishments. Beyond being a workplace, XYZ Corporation is your professional family.
+  
+  Forge your path with XYZ Corporation, an eminent frontrunner poised to redefine possibilities. Your contributions will catalyze industries and inspire generations. Align your aspirations with us, and be part of a legacy that reshapes the world.
+  
+  XYZ Corporation, a vanguard in the industry, invites you to make your mark. Here, you'll be part of a peerless collective, conquering extraordinary challenges and propelling progress. Your journey to success finds its ideal beginning here.
+  
+  Embark on a transformative journey with us at XYZ Corporation, where brilliance thrives. Join a team that has etched its name in industries, influenced communities, and nurtured careers. As you contribute, your potential will amplify, setting new benchmarks for excellence.`,
   "orgType": "Enterprise",
   "orgSize": "1 - 10 Employees",
   "industry": {
