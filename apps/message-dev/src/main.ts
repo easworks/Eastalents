550889841a28809bf0fe041e2a649de30704a814
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-08-29 08:03:57 +0530

    set a foundation for the message dev app

--- apps/message-dev/src/main.ts
+++ apps/message-dev/src/main.ts
@@ -1,1 +1,16 @@
-console.log('Hello World');
+import fastify from "fastify";
+import fastifyIO from "fastify-socket.io";
+
+const server = fastify();
+server.register(fastifyIO);
+
+server.ready().then(() => {
+  console.debug('ready');
+  // we need to wait for the server to be ready, else `server.io` is undefined
+
+  server.io.on("connection", (socket) => {
+    console.debug('new socket');
+  });
+});
+
+server.listen({ port: 4201 });
