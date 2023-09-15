01fd9b88e4950477f06ec107265f3b56c4f8a14b
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-09-15 10:25:26 +0530

    delete unused component

--- apps/web-client/src/public/software/software.page.ts
+++ apps/web-client/src/public/software/software.page.ts
@@ -5,10 +5,9 @@ import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
 import { sortString } from '@easworks/app-shell/utilities/sort';
 import { Domain, SoftwareProduct } from '@easworks/models';
-import { DomainSoftwareSelectorComponent } from '../common/domain-software-selector.component';
-import { RoleLinkListContainerComponent } from '../common/role-link-list-container.component';
-import { RoleSoftwareTalentComponent } from '../common/role-software-talent.component';
 import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { DomainSoftwareSelectorComponent } from '../common/domain-software-selector.component';
+import { RoleSoftwareTalentComponent } from '../common/role-software-talent.component';
 
 @Component({
   standalone: true,
@@ -18,7 +17,6 @@ import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     LottiePlayerDirective,
-    RoleLinkListContainerComponent,
     RoleSoftwareTalentComponent,
     DomainSoftwareSelectorComponent,
     ImportsModule,
