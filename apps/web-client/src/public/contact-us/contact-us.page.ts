f565d558fd95cd79c10267878c5f04493fbc83c4
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-09-15 15:34:52 +0530

    update

--- apps/web-client/src/public/contact-us/contact-us.page.ts
+++ apps/web-client/src/public/contact-us/contact-us.page.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatRippleModule } from '@angular/material/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormImportsModule } from '@easworks/app-shell/common/form.imports.module';
+import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
+import { pattern } from '@easworks/models';
 
 @Component({
   standalone: true,
@@ -10,8 +13,29 @@ import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.
   styleUrls: [],
   imports: [
     LottiePlayerDirective,
-    MatRippleModule
+    ImportsModule,
+    FormImportsModule
   ]
 })
 export class ContactUsPageComponent {
+  protected readonly form = new FormGroup({
+    name: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.pattern(pattern.email)],
+      nonNullable: true
+    }),
+    subject: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+    message: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required]
+    }),
+  }, {
+    updateOn: 'submit'
+  });
 }
