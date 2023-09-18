765f2252e222d7d0a569df51b54a264d97a142ca
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-09-18 18:11:34 +0530

    update contact us form

--- apps/web-client/src/public/contact-us/contact-us.page.ts
+++ apps/web-client/src/public/contact-us/contact-us.page.ts
@@ -1,8 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, INJECTOR, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { CSCApi, Country } from '@easworks/app-shell/api/csc.api';
+import { DropDownIndicatorComponent } from '@easworks/app-shell/common/drop-down-indicator.component';
+import { controlValue$ } from '@easworks/app-shell/common/form-field.directive';
 import { FormImportsModule } from '@easworks/app-shell/common/form.imports.module';
 import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
+import { PhoneCodeOption, filterCountryCode, getPhoneCodeOptions, updatePhoneValidatorEffect } from '@easworks/app-shell/common/phone-code';
+import { generateLoadingState } from '@easworks/app-shell/state/loading';
+import { sortString } from '@easworks/app-shell/utilities/sort';
 import { pattern } from '@easworks/models';
 
 @Component({
@@ -14,28 +22,86 @@ import { pattern } from '@easworks/models';
   imports: [
     LottiePlayerDirective,
     ImportsModule,
-    FormImportsModule
+    FormImportsModule,
+    MatAutocompleteModule,
+    DropDownIndicatorComponent
   ]
 })
 export class ContactUsPageComponent {
-  protected readonly form = new FormGroup({
-    name: new FormControl('', {
-      nonNullable: true,
-      validators: [Validators.required]
-    }),
-    email: new FormControl('', {
-      validators: [Validators.required, Validators.pattern(pattern.email)],
-      nonNullable: true
-    }),
-    subject: new FormControl('', {
-      nonNullable: true,
-      validators: [Validators.required]
-    }),
-    message: new FormControl('', {
-      nonNullable: true,
-      validators: [Validators.required]
-    }),
-  }, {
-    updateOn: 'submit'
-  });
+
+  private readonly injector = inject(INJECTOR);
+  private readonly api = {
+    csc: inject(CSCApi)
+  };
+
+  protected readonly trackBy = {
+    country: (_: number, c: Country) => c.iso2,
+  } as const;
+
+  private readonly allCountries = this.api.csc.allCountries();
+  protected readonly loading = generateLoadingState<[
+    'countries'
+  ]>();
+
+  protected readonly contact = this.initForm();
+
+  private initForm() {
+    const form = new FormGroup({
+      name: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required]
+      }),
+      email: new FormControl('', {
+        validators: [Validators.required, Validators.pattern(pattern.email)],
+        nonNullable: true
+      }),
+      phoneNumber: new FormGroup({
+        code: new FormControl(''),
+        number: new FormControl('')
+      }, {
+        updateOn: 'blur'
+      }),
+      subject: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required]
+      }),
+      message: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required]
+      }),
+    }, {
+      updateOn: 'submit'
+    });
+
+    const values = {
+      code$: toSignal(controlValue$(form.controls.phoneNumber.controls.code), { requireSync: true })
+    } as const;
+
+    updatePhoneValidatorEffect(form.controls.phoneNumber);
+
+    const allOptions = {
+      countryCodes$: signal<PhoneCodeOption[]>([])
+    } as const;
+
+    const filteredOptions = {
+      code$: filterCountryCode(allOptions.countryCodes$, values.code$)
+    } as const;
+
+    {
+      this.loading.add('countries');
+      this.allCountries
+        .then(countries => {
+          const options = getPhoneCodeOptions(countries).sort((a, b) => sortString(a.name, b.name));
+
+          allOptions.countryCodes$.set(options);
+
+        })
+        .finally(() => this.loading.delete('countries'));
+    }
+
+    return {
+      form,
+      options: filteredOptions
+    } as const;
+  }
 }
--- libs/app-shell/src/common/telephone-form/telephone-form.component.ts
+++ libs/app-shell/src/common/telephone-form/telephone-form.component.ts
@@ -1,11 +1,57 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, HostBinding, Input, Signal, computed, signal } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { DropDownIndicatorComponent } from '../drop-down-indicator.component';
+import { FormImportsModule } from '../form.imports.module';
+import { PhoneCodeForm, PhoneCodeOption } from '../phone-code';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { controlValue$ } from '../form-field.directive';
 
 @Component({
   standalone: true,
   selector: 'telephone-form',
   templateUrl: './telephone-form.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    CommonModule,
+    FormImportsModule,
+    MatAutocompleteModule,
+    DropDownIndicatorComponent
+  ]
 })
 export class TelephoneFormComponent {
+  @HostBinding()
+  private readonly class = 'block';
 
+  @Input({ required: true })
+  form!: PhoneCodeForm;
+
+  @Input({ required: true })
+  phoneCodeOptions$!: Signal<PhoneCodeOption[]>;
+
+  private code$ = toSignal(controlValue$(this.form.controls.number), { requireSync: true });
+  // protected filteredOptions$ = computed();
+
+
+  static readonly createForm = {
+    required: () => {
+      return new FormGroup({
+        code: new FormControl('', {
+          nonNullable: true,
+          validators: [Validators.required]
+        }),
+        number: new FormControl('', {
+          nonNullable: true,
+          validators: [Validators.required]
+        })
+      });
+    },
+    optional: () => {
+      return new FormGroup({
+        code: new FormControl(''),
+        number: new FormControl('')
+      });
+    }
+  } as const;
 }
