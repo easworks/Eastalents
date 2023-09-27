6665997836c164b49a3276df716ed297be42f8e5
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-09-27 15:39:37 +0530

    update

--- apps/web-client/src/public/help-center/help-center-group.page.ts
+++ apps/web-client/src/public/help-center/help-center-group.page.ts
@@ -1,23 +1,31 @@
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, computed, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { ImportsModule } from '@easworks/app-shell/common/imports.module';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { HelpGroup } from './data';
 
 @Component({
   standalone: true,
   selector: 'help-center-group-page',
   templateUrl: './help-center-group.page.html',
-  styleUrls: [],
+  styleUrls: ['./help-center-group.page.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    ImportsModule
+    ImportsModule,
+    RouterModule
   ]
 })
 export class HelpCenterGroupPageComponent {
   constructor() {
     this.listenToRouteChanges();
   }
+
+  @HostBinding()
+  private readonly class = 'page';
+  protected readonly icons = {
+    faChevronRight
+  } as const;
 
   protected readonly group$ = signal<HelpGroup>({
     title: '',
@@ -27,6 +35,37 @@ export class HelpCenterGroupPageComponent {
   });
 
   private readonly fragment$ = signal<string | null>(null);
+  protected readonly item$ = computed(() => {
+    const g = this.group$();
+    const f = this.fragment$();
+    if (!f) return g.items[0];
+
+    const found = g.items.find(i => i.slug === f);
+    if (!found) return g.items[0];
+
+    return found;
+  });
+
+  protected readonly breadcrumb$ = computed(() => {
+    const g = this.group$();
+    const i = this.item$();
+
+    return [
+      {
+        text: 'Help Center',
+        link: '/help-center'
+      },
+      {
+        text: g.title,
+        link: `/help-center/${g.slug}`
+      },
+      {
+        text: i.title,
+        link: `/help-center/${g.slug}`,
+        fragment: i.slug
+      }
+    ];
+  });
 
   private listenToRouteChanges() {
     const route = inject(ActivatedRoute);
@@ -37,6 +76,6 @@ export class HelpCenterGroupPageComponent {
       });
 
     route.fragment.pipe(takeUntilDestroyed())
-      .subscribe(f => this.fragment$.set(f || null));
+      .subscribe(f => this.fragment$.set(f));
   }
 }
