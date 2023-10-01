93bfacd428748359cf9f002340c36d9d9b45a9f9
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2023-10-02 04:50:22 +0530

    used featured-domains component

--- apps/web-client/src/public/easworks-talent/easworks-talent.page.ts
+++ apps/web-client/src/public/easworks-talent/easworks-talent.page.ts
@@ -1,11 +1,7 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
-import { DomainsApi } from '@easworks/app-shell/api/domains.api';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
-import { DomainState } from '@easworks/app-shell/state/domains';
-import { generateLoadingState } from '@easworks/app-shell/state/loading';
-import { fromPromise } from '@easworks/app-shell/utilities/to-promise';
-import { SoftwareTilesContainerComponent } from '../common/software-tiles-container.component';
+import { FeaturedDomainsComponent } from '../common/featured-domains.component';
 
 @Component({
   standalone: true,
@@ -16,61 +12,7 @@ import { SoftwareTilesContainerComponent } from '../common/software-tiles-contai
   imports: [
     LottiePlayerDirective,
     ImportsModule,
-    SoftwareTilesContainerComponent
+    FeaturedDomainsComponent
   ]
 })
-export class EasworksTalentPageComponent {
-  private readonly api = {
-    domains: inject(DomainsApi)
-  } as const;
-  private readonly domainState = inject(DomainState);
-
-  private readonly loading = generateLoadingState<[
-    'featured domains'
-  ]>();
-
-  protected readonly featuredDomains = this.initFeaturedDomains();
-
-  private initFeaturedDomains() {
-
-    const list$ = fromPromise(this.api.domains.featuredDomains(), []);
-
-
-    const featured$ = computed(() => {
-      const list = list$();
-      const domainMap = this.domainState.domains.map$();
-      const productMap = this.domainState.products.map$();
-
-      if (domainMap.size === 0 || productMap.size === 0)
-        return [];
-
-      const featured = list.map(l => {
-        const domain = domainMap.get(l.domain);
-        if (!domain)
-          throw new Error(`module '${l.domain}' not fond`);
-
-        const products = l.products
-          .map(p => {
-            const product = productMap.get(p);
-            if (!product)
-              throw new Error(`module '${l.domain}' has no product '${p}'`);
-            return product;
-          });
-
-        return {
-          ...domain,
-          products
-        } as const;
-      });
-
-      return featured;
-    });
-
-    const loading$ = computed(() => featured$().length === 0);
-
-    return {
-      domains$: featured$,
-      loading$
-    };
-  }
-}
+export class EasworksTalentPageComponent { }
--- apps/web-client/src/public/for-freelancer/for-freelancer.page.ts
+++ apps/web-client/src/public/for-freelancer/for-freelancer.page.ts
@@ -1,15 +1,12 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { ActivatedRoute } from '@angular/router';
-import { DomainsApi } from '@easworks/app-shell/api/domains.api';
 import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
 import { HelpGroup } from '@easworks/app-shell/services/help';
-import { DomainState } from '@easworks/app-shell/state/domains';
-import { fromPromise } from '@easworks/app-shell/utilities/to-promise';
 import { FAQGroup, FAQListComponent } from '../common/faq-list.component';
-import { SoftwareTilesContainerComponent } from '../common/software-tiles-container.component';
+import { FeaturedDomainsComponent } from '../common/featured-domains.component';
 
 @Component({
   standalone: true,
@@ -22,7 +19,7 @@ import { SoftwareTilesContainerComponent } from '../common/software-tiles-contai
     LottiePlayerDirective,
     MatExpansionModule,
     FAQListComponent,
-    SoftwareTilesContainerComponent
+    FeaturedDomainsComponent
   ]
 })
 export class ForFreelancerPageComponent {
@@ -43,55 +40,6 @@ export class ForFreelancerPageComponent {
       });
   }
 
-  private readonly api = {
-    domains: inject(DomainsApi)
-  } as const;
-  private readonly domainState = inject(DomainState);
-
-  protected readonly featuredDomains = this.initFeaturedDomains();
-
   protected readonly faqs$ = signal<FAQGroup[]>([]);
 
-  private initFeaturedDomains() {
-
-    const list$ = fromPromise(this.api.domains.featuredDomains(), []);
-
-
-    const featured$ = computed(() => {
-      const list = list$();
-      const domainMap = this.domainState.domains.map$();
-      const productMap = this.domainState.products.map$();
-
-      if (domainMap.size === 0 || productMap.size === 0)
-        return [];
-
-      const featured = list.map(l => {
-        const domain = domainMap.get(l.domain);
-        if (!domain)
-          throw new Error(`module '${l.domain}' not fond`);
-
-        const products = l.products
-          .map(p => {
-            const product = productMap.get(p);
-            if (!product)
-              throw new Error(`module '${l.domain}' has no product '${p}'`);
-            return product;
-          });
-
-        return {
-          ...domain,
-          products
-        } as const;
-      });
-
-      return featured;
-    });
-
-    const loading$ = computed(() => featured$().length === 0);
-
-    return {
-      domains$: featured$,
-      loading$
-    };
-  }
 }
