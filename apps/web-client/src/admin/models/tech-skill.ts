47058889978ee3abab31f84b855649adbfb23b02
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-02-09 23:20:00 +0530

    add typings for tech group

--- apps/web-client/src/admin/models/tech-skill.ts
+++ apps/web-client/src/admin/models/tech-skill.ts
@@ -1,5 +1,27 @@
-export interface TechSkill {
+export interface GenericTechSkill {
   id: string,
   name: string;
-  generic: boolean;
+  generic: true;
 }
+
+export interface NonGenericTechSkill extends Omit<GenericTechSkill, 'generic'> {
+  generic: false;
+}
+
+export type TechSkill = GenericTechSkill | NonGenericTechSkill;
+
+
+export interface GenericTechGroup {
+  id: string,
+  name: string;
+  generic: true;
+  tech: GenericTechSkill[];
+}
+
+export interface NonGenericTechGroup extends Omit<GenericTechGroup, 'generic' | 'tech'> {
+  generic: false;
+  softwareId: string;
+  tech: TechSkill[];
+}
+
+export type TechGroup = GenericTechGroup | NonGenericTechGroup;
