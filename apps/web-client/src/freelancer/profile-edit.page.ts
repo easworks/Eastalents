585f5aac15e6bace42b9e87f1ae3615eabf86b7b
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-02-16 05:19:09 +0530

    update stepper indicator

--- apps/web-client/src/freelancer/profile-edit.page.ts
+++ apps/web-client/src/freelancer/profile-edit.page.ts
@@ -285,6 +285,7 @@ export class FreelancerProfileEditPageComponent implements OnInit {
       label: StepGroupID;
       enabled$: Signal<boolean>;
       completed$: Signal<boolean>;
+      current$: Signal<boolean>;
       click: () => void;
     };
 
@@ -330,6 +331,7 @@ export class FreelancerProfileEditPageComponent implements OnInit {
         label,
         enabled$: computed(() => true),
         completed$: computed(() => steps.every(step => isValidStep(step))),
+        current$: computed(() => steps.includes(this.stepper.step$())),
         click: () => this.stepper.step$.set(steps[0])
       }));
 
