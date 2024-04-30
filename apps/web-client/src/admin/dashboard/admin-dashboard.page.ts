173922515d5da5f0f3619747ec1fe3f0678ba13b
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-04-30 06:21:32 +0530

    update dashboard page

--- apps/web-client/src/admin/dashboard/admin-dashboard.page.ts
+++ apps/web-client/src/admin/dashboard/admin-dashboard.page.ts
@@ -1,9 +1,12 @@
 import { Component, HostBinding } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 
 @Component({
   standalone: true,
   selector: 'admin-dashboard',
-  templateUrl: './admin-dashboard.page.html'
+  templateUrl: './admin-dashboard.page.html',
+  imports: [ImportsModule, RouterModule]
 })
 export default class AdminDashboardPageComponent {
   @HostBinding() private readonly class = 'page';
