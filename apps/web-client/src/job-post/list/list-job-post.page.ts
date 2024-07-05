e113173fcd60aaf296260da2e69288690f2250dd
skv10 <sameershukla1999@gmail.com>
2024-07-05 05:18:04 +0400

    Tab Switching Change of Job List Page

--- apps/web-client/src/job-post/list/list-job-post.page.ts
+++ apps/web-client/src/job-post/list/list-job-post.page.ts
@@ -1,17 +1,17 @@
 import { TextFieldModule } from '@angular/cdk/text-field';
-import { ChangeDetectionStrategy, Component, HostBinding, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, INJECTOR, OnInit, computed, effect, inject, input } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatPseudoCheckboxModule } from '@angular/material/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormImportsModule } from '@easworks/app-shell/common/form.imports.module';
 import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 import { JobPost } from '@easworks/models';
 import { ListCardPageComponent } from '../list-card/list-card.page';
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
 
 @Component({
     selector: 'employer-list-job-post',
@@ -31,16 +31,47 @@ import { MatTabsModule } from '@angular/material/tabs';
         RouterModule,
         MatTooltipModule,
         ListCardPageComponent,
-        MatTabsModule
-    ]
+        MatTabsModule,
+    ],
 })
-export class ListJobPostPageComponent {
+export class ListJobPostPageComponent implements OnInit {
 
     @HostBinding() private readonly class = 'page';
 
+    private readonly injector = inject(INJECTOR);
+
+    private readonly router = inject(Router);
 
     protected readonly routeData$ = input.required<JobPost[]>({ alias: 'data' });
+    protected readonly editTab$ = input<Tab | null>(null, { alias: 'tab' });
 
     protected readonly list$ = computed(() => this.routeData$());
 
+    protected selectedIndex = 0;
+
+
+    ngOnInit(): void {
+
+        effect(() => {
+            const tab = this.editTab$();
+
+            if (tab === "Applied") {
+                this.selectedIndex = 1;
+            }
+            else {
+                this.selectedIndex = 0;
+            }
+
+
+        }, { injector: this.injector });
+    }
+
+
+    SelectedTabChange(event: MatTabChangeEvent) {
+        console.log(event);
+        const url = `job-post/list/${event.tab.textLabel}`;
+        this.router.navigateByUrl(url);
+    }
 }
+
+type Tab = 'Available' | 'Applied';
