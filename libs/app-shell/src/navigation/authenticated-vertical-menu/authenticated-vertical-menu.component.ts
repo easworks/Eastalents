ae777af2da512f6e958e6a1416cf0fa68c48ee9b
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-05 05:08:00 +0400

    create empty component

---
+++ libs/app-shell/src/navigation/authenticated-vertical-menu/authenticated-vertical-menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'authenticated-vertical-menu',
+  templateUrl: './authenticated-vertical-menu.component.html',
+  styleUrl: './authenticated-vertical-menu.component.less'
+})
+export class AuthenticateVerticalMenuComponent {
+
+}
