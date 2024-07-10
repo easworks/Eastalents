58b61d3297738017f23db5f60ce546d25e8a2521
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-10 22:04:45 +0400

    link domains from product side

--- apps/web-client/src/admin/state/admin-data.ts
+++ apps/web-client/src/admin/state/admin-data.ts
@@ -85,7 +85,9 @@ const feature = createFeature({
       };
 
       state.domains = adapters.domain.setMany(payload.domains, state.domains);
-      state.softwareProducts = adapters.softwareProduct.setMany(payload.softwareProducts, state.softwareProducts);
+      state.softwareProducts = adapters.softwareProduct.setMany(
+        payload.softwareProducts.map(p => ({ ...p, domains: [] })),
+        state.softwareProducts);
       state.techSkills = adapters.techSkill.setMany(payload.techSkills, state.techSkills);
       state.techGroups = adapters.techGroup.setMany(
         payload.techGroups.map(g => ({ ...g, skills: [] })),
@@ -97,6 +99,15 @@ const feature = createFeature({
           if (!group)
             throw new Error('invalid operation');
           group.skills.push(skill.id);
+        }
+      }
+
+      for (const domain of payload.domains) {
+        for (const id of domain.products) {
+          const product = state.softwareProducts.entities[id];
+          if (!product)
+            throw new Error('invalid operation');
+          product.domains.push(domain.id);
         }
       }
 
