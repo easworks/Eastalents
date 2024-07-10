bfb6e9f93b42b82a5932a3076a22d14cf692eb41
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-10 08:22:13 +0400

    partial refactor

--- apps/web-client/src/employer/my-profile/my-account.component.ts
+++ apps/web-client/src/employer/my-profile/my-account.component.ts
@@ -1,30 +1,21 @@
-import { Component, HostBinding } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, HostBinding, signal } from '@angular/core';
+import { AccountEditPopupComponent } from './account-edit-popup/account-edit-popup.component';
 import { AccountSecurityComponent } from './account-security/account-security.component';
 import { ManageCollabratorComponent } from './manage-collabrator/manage-collabrator.component';
-import { AccountEditPopupComponent } from './account-edit-popup/account-edit-popup.component';
 
 @Component({
   selector: 'app-my-account',
   standalone: true,
-  imports: [ CommonModule,AccountSecurityComponent,ManageCollabratorComponent,AccountEditPopupComponent],
+  imports: [CommonModule, AccountSecurityComponent, ManageCollabratorComponent, AccountEditPopupComponent],
   templateUrl: './my-account.component.html',
-  styleUrl: './my-account.component.css'
+  styleUrl: './my-account.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MyAccountComponent {
-  @HostBinding() private readonly class = "page"
-  activeTab: string = 'manage-collab';
-  isEditPopupVisible: boolean = false;
-
-  setActiveTab(tab: string) {
-    this.activeTab = tab;
-  }
-
-  isActive(tab: string) {
-    return this.activeTab === tab;
-  }
-  
-  setIsEditPopupVisible(value: boolean) {
-    this.isEditPopupVisible = value;
-  }
+  @HostBinding() private readonly class = "page";
+  protected readonly activeTab$ = signal<Tab>('manage-collab');
+  protected readonly isEditPopupVisible$ = signal(false);
 }
+
+type Tab = 'manage-collab' | 'security';
