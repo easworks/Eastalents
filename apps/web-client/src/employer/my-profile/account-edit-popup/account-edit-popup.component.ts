a867de290dc4214adee1078ad569f1b2c616f8b1
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-10 07:57:06 +0400

    partial refactor of account popup

--- apps/web-client/src/employer/my-profile/account-edit-popup/account-edit-popup.component.ts
+++ apps/web-client/src/employer/my-profile/account-edit-popup/account-edit-popup.component.ts
@@ -1,15 +1,28 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormImportsModule } from '@easworks/app-shell/common/form.imports.module';
+import { ImportsModule } from '@easworks/app-shell/common/imports.module';
 
 @Component({
   selector: 'app-account-edit-popup',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './account-edit-popup.component.html',
   styleUrl: './account-edit-popup.component.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ImportsModule,
+    FormImportsModule
+  ],
 })
 export class AccountEditPopupComponent {
   @Output() closePopup = new EventEmitter<void>();
+
+  protected readonly form = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    phone: new FormControl(""),
+    jobTitle: new FormControl(""),
+  });
 
   onClosePopup() {
     this.closePopup.emit();
