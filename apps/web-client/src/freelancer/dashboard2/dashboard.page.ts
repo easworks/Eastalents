aff4c4605ca87d46bbb660626264d226d4ab2ab9
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-11 23:25:48 +0400

    use less-file for styling metrics

--- apps/web-client/src/freelancer/dashboard2/dashboard.page.ts
+++ apps/web-client/src/freelancer/dashboard2/dashboard.page.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faEnvelope, faPaperPlane, faSuitcase, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
+  standalone: true,
   selector: 'new-dashboard-page',
   templateUrl: './dashboard.page.html',
-  standalone: true,
+  styleUrl: './dashboard.page.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
@@ -14,7 +15,29 @@ import { faEnvelope, faPaperPlane, faSuitcase, faUser } from '@fortawesome/free-
   ]
 })
 export class DashboardComponent {
+
+  @HostBinding()
+  private readonly class = 'page @container';
+
   protected readonly icons = {
     faSuitcase, faPaperPlane, faUser, faEnvelope
   } as const;
+
+  protected readonly metrics = mockMetrics;
+
+
 }
+
+interface ProfileMetrics {
+  scheduledInterviews: number;
+  applicationCount: number;
+  profileViewCount: number;
+  unreadMessages: number;
+}
+
+const mockMetrics: ProfileMetrics = {
+  scheduledInterviews: 76,
+  applicationCount: 65,
+  profileViewCount: 35673,
+  unreadMessages: 83
+};
