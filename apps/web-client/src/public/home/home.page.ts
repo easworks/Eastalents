149bb29b58b523f6066ced09a30e0635a46b54f5
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-12 05:10:45 +0400

    updated home page
    - updated the enterprise-app-dev section of the home-page
    - removed unused assets
    - removed unused tailwind config options

--- apps/web-client/src/public/home/home.page.ts
+++ apps/web-client/src/public/home/home.page.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { facCertificateCheck, facCornerRibbon } from 'custom-icons';
 
 @Component({
   standalone: true,
@@ -7,11 +10,44 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './home.page.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-
+    FontAwesomeModule
   ]
 })
 export class HomePageComponent {
+  protected readonly icons = {
+    faLocationDot,
+    facCertificateCheck,
+    facCornerRibbon
+  } as const;
 
+  protected readonly madeEasySection = {
+    devs: [
+      {
+        name: 'Arizona Cardinals',
+        role: 'Full-stack Developer',
+        image: '/assets/public/home/enterprise-app-dev/dev1.png',
+        location: 'Puna, India',
+        badge: 'bg-emerald-400',
+        highlight: false
+      },
+      {
+        name: 'Green Bay Packers',
+        role: 'Full-stack Developer',
+        image: '/assets/public/home/enterprise-app-dev/dev2.png',
+        location: 'Puna, India',
+        badge: 'bg-primary-400',
+        highlight: true
+      },
+      {
+        name: 'Baltimore Ravens',
+        role: 'Full-stack Developer',
+        image: '/assets/public/home/enterprise-app-dev/dev3.png',
+        location: 'Puna, India',
+        badge: 'bg-yellow-400',
+        highlight: false
+      }
+    ]
+  } as const;
 
 
 }
