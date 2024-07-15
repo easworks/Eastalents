c9bd36d9594f94ff96760bb59b3afab6c8a638e3
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-15 11:25:38 +0400

    create hire-contractors page files

---
+++ apps/web-client/src/public/service-type/hire-contractors/hire-contractors.page.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'service-type-hire-contractors-page',
+  templateUrl: './hire-contractors.page.html',
+  styleUrl: './hire-contractors.page.less',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: []
+})
+export class HireContractorsPageComponent {
+
+}
