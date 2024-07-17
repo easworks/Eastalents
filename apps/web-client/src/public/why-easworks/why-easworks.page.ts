833076bf5bc2673986912636b3bc202253046cea
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-17 11:23:31 +0400

    empty the why-easworks-page

--- apps/web-client/src/public/why-easworks/why-easworks.page.ts
+++ apps/web-client/src/public/why-easworks/why-easworks.page.ts
@@ -1,10 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
-import { LottiePlayerDirective } from '@easworks/app-shell/common/lottie-player.directive';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faCheckCircle, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
-import { UseCaseTilesContainerComponent } from '../common/use-case-tiles-container.component';
-import { MatRippleModule } from '@angular/material/core';
 
 @Component({
   standalone: true,
@@ -13,16 +7,9 @@ import { MatRippleModule } from '@angular/material/core';
   styleUrls: ['./why-easworks.page.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    LottiePlayerDirective,
-    MatTabsModule,
-    UseCaseTilesContainerComponent,
-    FontAwesomeModule,
-    MatRippleModule
+
   ]
 })
 export class WhyEasworksPageComponent {
-  protected readonly icons = {
-    faXmarkCircle,
-    faCheckCircle
-  } as const;
+
 }
