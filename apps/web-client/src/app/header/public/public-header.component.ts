fdce6d213c25bb4c8d1e9f89717cf1e9d53bd0ea
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-17 06:55:15 +0400

    update public header

--- apps/web-client/src/app/header/public/public-header.component.ts
+++ apps/web-client/src/app/header/public/public-header.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, inject } from '@angular/core';
 import { publicMenu } from '../../menu-items/public';
+import { RouterModule } from '@angular/router';
+import { MatDividerModule } from '@angular/material/divider';
+import { faHighTechFourLeaf } from 'custom-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatRippleModule } from '@angular/material/core';
+import { Store } from '@ngrx/store';
+import { sidebarActions } from '@easworks/app-shell/state/ui';
 
 @Component({
   standalone: true,
@@ -7,9 +14,26 @@ import { publicMenu } from '../../menu-items/public';
   templateUrl: './public-header.component.html',
   styleUrl: './public-header.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: []
+  imports: [
+    RouterModule,
+    MatDividerModule,
+    FontAwesomeModule,
+    MatRippleModule
+  ]
 })
 export class AppPublicHeaderComponent {
+  private readonly store = inject(Store);
+
+  @HostBinding()
+  private readonly class = 'flex h-full gap-4 items-center bg-black p-1 px-4 overflow-clip';
+
+  protected readonly icons = {
+    faHighTechFourLeaf
+  } as const;
 
   protected readonly menuItems = publicMenu.main;
+
+  protected toggleSidebar() {
+    this.store.dispatch(sidebarActions.toggleExpansion());
+  }
 }
