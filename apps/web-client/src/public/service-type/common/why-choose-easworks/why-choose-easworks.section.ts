71b27e77d9a9b8d3584ae6f13690889f3ffda63e
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-18 11:47:22 +0400

    move 2 sections to separate component

---
+++ apps/web-client/src/public/service-type/common/why-choose-easworks/why-choose-easworks.section.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'service-type-why-choose-easworks-section',
+  templateUrl: './why-choose-easworks.section.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: []
+})
+export class ServiceTypeWhyChooseEasworksSectionComponent {
+
+}
--- apps/web-client/src/public/service-type/hire-contractors/hire-contractors.page.ts
+++ apps/web-client/src/public/service-type/hire-contractors/hire-contractors.page.ts
@@ -1,4 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ServiceTypeHiringStepsSectionComponent } from '../common/hiring-steps/hiring-steps.section';
+import { ServiceTypeWhyChooseEasworksSectionComponent } from '../common/why-choose-easworks/why-choose-easworks.section';
 
 @Component({
   standalone: true,
@@ -6,7 +8,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   templateUrl: './hire-contractors.page.html',
   styleUrl: './hire-contractors.page.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: []
+  imports: [
+    ServiceTypeHiringStepsSectionComponent,
+    ServiceTypeWhyChooseEasworksSectionComponent
+  ]
 })
 export class HireContractorsPageComponent {
 
