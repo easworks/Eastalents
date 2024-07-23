76985dbcef842077dffe01c1ba56ef245d76af6b
Navjot Kaur <navjot.kaur@ensuesoft.com>
2024-07-23 18:47:08 +0530

    Added common tabs content for main view

--- apps/web-client/src/public/why-easworks/why-easworks.page.ts
+++ apps/web-client/src/public/why-easworks/why-easworks.page.ts
@@ -1,6 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatTabsModule } from '@angular/material/tabs';
+import { ServiceTypeWhyTabContentSectionComponent } from './tab-contents/why-tab-content/why-tab-content.section';
 import { ServiceTypeGetTopEnterpriseSectionComponent } from './common/get-top-enterprise/get-top-enterprise.section';
+import { ServiceTypeHowTabContentSectionComponent } from './tab-contents/how-tab-content/how-tab-content.section';
+import { ServiceTypeWhatTabContentSectionComponent } from './tab-contents/what-tab-content/what-tab-content.section';
 
 @Component({
   standalone: true,
@@ -8,6 +11,12 @@ import { ServiceTypeGetTopEnterpriseSectionComponent } from './common/get-top-en
   templateUrl: './why-easworks.page.html',
   styleUrls: ['./why-easworks.page.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [MatTabsModule, ServiceTypeGetTopEnterpriseSectionComponent],
+  imports: [
+    MatTabsModule,
+    ServiceTypeWhyTabContentSectionComponent,
+    ServiceTypeGetTopEnterpriseSectionComponent,
+    ServiceTypeHowTabContentSectionComponent,
+    ServiceTypeWhatTabContentSectionComponent,
+  ],
 })
-export class WhyEasworksPageComponent { }
+export class WhyEasworksPageComponent {}
