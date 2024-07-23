5b56d063683e5896a065057ec9b3581ebba18390
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-23 18:12:54 +0400

    demo naming convention

---
+++ apps/web-client/src/public/use-cases/digital-transformation/overview/digital-transformation-overview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'use-cases-digital-transformation-overview',
+  template: '',
+})
+export class DigitalTransformationOverviewComponent {
+
+}
