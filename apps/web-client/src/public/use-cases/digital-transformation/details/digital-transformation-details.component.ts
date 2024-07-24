274f09f3715927cd1a7c8777f576bb8ad32a2109
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-24 11:10:10 +0400

    create dynamic components for digital-transformation

---
+++ apps/web-client/src/public/use-cases/digital-transformation/details/digital-transformation-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'use-cases-digital-transformation-details',
+  template: 'This is the the digital-transformation details component',
+})
+export class DigitalTransformationDetailsComponent {
+
+}
---
+++ apps/web-client/src/public/use-cases/digital-transformation/hero/digital-transformation-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'use-cases-digital-transformation-hero',
+  template: 'This is the the digital-transformation hero component',
+})
+export class DigitalTransformationHeroComponent {
+
+}
