a880bb27ef449b88890b75a74d46d7e0a48377cb
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-27 14:27:29 +0400

    update

--- apps/accounts-client/src/app/header/app-header.component.ts
+++ apps/accounts-client/src/app/header/app-header.component.ts
@@ -1,9 +1,19 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { ImportsModule } from '@easworks/app-shell/common/imports.module';
+import { AccountWidgetComponent } from "../../account/account-widget/account.widget";
 
 @Component({
   standalone: true,
   selector: 'app-header',
   templateUrl: './app-header.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    ImportsModule,
+    AccountWidgetComponent
+  ]
 })
-export class AppHeaderComponent { }
+export class AppHeaderComponent {
+  @HostBinding()
+  private readonly class = 'flex h-full gap-4 items-center p-1 px-4';
+
+}
