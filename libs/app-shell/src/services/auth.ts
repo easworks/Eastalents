d5a041052980d0e18b165b52372c22e585a43a2c
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-07-30 08:54:35 +0400

    update

--- libs/app-shell/src/services/auth.ts
+++ libs/app-shell/src/services/auth.ts
@@ -1,11 +1,12 @@
 import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
 import type { EmailSignInInput } from 'models/validators/auth';
-import { from, switchMap, tap } from 'rxjs';
+import { from, map, switchMap, tap } from 'rxjs';
 import { AuthApi } from '../api/auth.api';
 import { UsersApi } from '../api/users.api';
 import { sleep } from '../utilities/sleep';
 import { AuthStorageService } from './auth.storage';
+import { authActions, getAuthUserFromModel } from '../state/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -17,25 +18,32 @@ export class AuthService {
     users: inject(UsersApi)
   } as const;
 
-  private readonly tokenService = inject(AuthStorageService);
+  private readonly storage = inject(AuthStorageService);
 
   public readonly signin = {
     email: (input: EmailSignInInput, returnUrl?: string) => {
       return this.api.auth.signin.email(input)
         .pipe(
-          switchMap(response => this.handleSignIn(response.access_token))
+          switchMap(response => this.handleSignIn(response.access_token, returnUrl))
         );
     }
   } as const;
 
-  private handleSignIn(token: string) {
-    return from(sleep(3000))
+  private handleSignIn(token: string, returnUrl?: string) {
+    return from(this.storage.token.set(token))
       .pipe(
-        switchMap(() => this.tokenService.token.set(token)),
-        tap(() => {
-          console.debug('completed sign in handling');
-
-        })
+        switchMap(() => this.api.users.self()),
+        switchMap(async self => {
+          const user = getAuthUserFromModel(self.user, self.permissionRecord);
+          await this.storage.user.set(user);
+          this.store.dispatch(authActions.updateUser({ payload: { user } }));
+          this.store.dispatch(authActions.signIn({
+            payload: {
+              needsOnboarding: false,
+              returnUrl: returnUrl || null
+            }
+          }));
+        }),
       );
   }
 }
