d1f6788a0079f386563743822dc22739172577be
Navjot Kaur <navjot.kaur@ensuesoft.com>
2024-08-06 14:21:29 +0530

    Minor fixes

--- apps/web-client/src/public/use-cases/software-customization/details/software-customization-details.component.ts
+++ apps/web-client/src/public/use-cases/software-customization/details/software-customization-details.component.ts
@@ -4,7 +4,6 @@ import { Component } from '@angular/core';
   standalone: true,
   selector: 'use-cases-software-customization-details',
   templateUrl: './software-customization-details.component.html',
-  styleUrl: './software-customization-details.component.less',
 
 })
 export class SoftwareCustomizationDetailsComponent {
