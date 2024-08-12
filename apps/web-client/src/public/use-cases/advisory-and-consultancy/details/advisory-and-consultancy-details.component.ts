e12f9ef3e0ee6cda95d197a9b0aae0052f94ba25
Navjot Kaur <navjot.kaur@ensuesoft.com>
2024-08-12 19:58:52 +0530

    Fixes in Use cases

--- apps/web-client/src/public/use-cases/advisory-and-consultancy/details/advisory-and-consultancy-details.component.ts
+++ apps/web-client/src/public/use-cases/advisory-and-consultancy/details/advisory-and-consultancy-details.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   standalone: true,
   selector: 'use-cases-advisory-and-consultancy-details',
   templateUrl: './advisory-and-consultancy-details.component.html',
+  styleUrl: './advisory-and-consultancy-details.component.less',
 })
 export class AdvisoryAndConsultancyDetailsComponent {
 
