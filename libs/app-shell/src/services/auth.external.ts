b5a9cbfdb2188bbe4dbd0dda52ed43d8ea09f0fa
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-08-16 06:44:55 +0530

    update

--- libs/app-shell/src/services/auth.external.ts
+++ libs/app-shell/src/services/auth.external.ts
@@ -14,23 +14,11 @@ export class AuthRedirect {
     google: {
       url: 'https://accounts.google.com/o/oauth2/v2/auth',
       scope: 'email profile',
-
-      // ram
-      // client_id: '243499270913-h4645hc46b9dqvg6ejaidpch2g6q863r.apps.googleusercontent.com',
-
-      // dibyodyuti
-      client_id: '1074824567437-5ts3rfo6s011cl4il6dev2qeqvm10lc0.apps.googleusercontent.com',
-
+      client_id: '579469758615-67877jg3b0tgiaik63p4b5qj20n8a2ng.apps.googleusercontent.com'
     },
     linkedin: {
       url: 'https://www.linkedin.com/oauth/v2/authorization',
-
-      //ram
-      // client_id: '77iv0gyzyvx8w2',
-
-      // dibyodyuti
-      client_id: '86zlnwx8vdlo7j',
-
+      client_id: '860msgodp05ins',
       scope: 'openid email profile'
     },
     github: {
