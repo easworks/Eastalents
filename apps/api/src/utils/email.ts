43f5d56848f9f21e37635838bf3d48e10507e3c2
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-08-19 04:31:28 +0530

    update

--- apps/api/src/utils/email.ts
+++ apps/api/src/utils/email.ts
@@ -1,6 +1,9 @@
-import path from 'node:path';
-import fs from 'node:fs/promises';
 import { User } from 'models/user';
+import fs from 'node:fs/promises';
+import path from 'node:path';
+import MailComposer from 'nodemailer/lib/mail-composer/index.js';
+import { GoogleApiError } from 'server-side/errors/definitions';
+import { easGoogle } from './google';
 
 function getTemplateFile(filename: string) {
   const templateFile = path.resolve(import.meta.filename, `../assets/templates/${filename}`);
@@ -13,42 +16,83 @@ function replaceVariables(template: string, inputs: [string, string][]) {
   }, template);
 }
 
-export const emailTemplates = {
-  verifyEmail: async (user: User, link: string) => {
-    return replaceVariables(
-      await getTemplateFile(`verify-email.html`),
-      [
+export class EmailSender {
+  static readonly compose = {
+    verifyEmail: async (user: User, code: string, link: string) => {
+      const tmp = await getTemplateFile('verify-email.html');
+      const html = replaceVariables(tmp, [
         ['user.firstName', user.firstName],
+        ['verification.code', code],
         ['verification.link', link]
-      ]
-    );
-  },
-  welcome: {
-    easworks: {
-      talent: async (user: User) => {
-        return replaceVariables(
-          await getTemplateFile('welcome/talent.html'),
-          [
-            ['user.firstName', user.firstName],
-          ]
-        );
-      },
-      employer: async (user: User) => {
-        return replaceVariables(
-          await getTemplateFile('welcome/employer.html'),
-          [
-            ['user.firstName', user.firstName],
-          ]
-        );
-      }
+      ]);
+
+      return new MailComposer({
+        to: user.email,
+        subject: 'Email Verification',
+        html
+      });
     },
-    easdevhub: async (user: User) => {
-      return replaceVariables(
-        await getTemplateFile('welcome/easdevhub.html'),
-        [
+    welcome: {
+      easworks: {
+        talent: async (user: User) => {
+          const tmp = await getTemplateFile('welcome/talent.html');
+          const html = replaceVariables(tmp, [
+            ['user.firstName', user.firstName],
+          ]);
+
+          return new MailComposer({
+            to: user.email,
+            subject: 'Welcome to Easworks',
+            html
+          });
+        },
+        employer: async (user: User) => {
+          const tmp = await getTemplateFile('welcome/employer.html');
+          const html = replaceVariables(tmp, [
+            ['user.firstName', user.firstName],
+          ]);
+
+          return new MailComposer({
+            to: user.email,
+            subject: 'Welcome to Easworks',
+            html
+          });
+        },
+      },
+      easdevhub: async (user: User) => {
+        const tmp = await getTemplateFile('welcome/easdevhub.html');
+        const html = replaceVariables(tmp, [
           ['user.firstName', user.firstName],
-        ]
-      );
+        ]);
+
+        return new MailComposer({
+          to: user.email,
+          subject: 'Welcome to Easdevhub',
+          html
+        });
+      }
     }
+  } as const;
+
+  static async sendEmail(compose: MailComposer, senderId: string) {
+
+    const raw = await compose.compile().build()
+      .then(buf => buf.toString('base64'));
+
+    // {
+    //   const QP = await import('libqp');
+    //   const x = QP.decode(Buffer.from(raw, 'base64').toString('utf-8')).toString();
+    //   console.debug(x);
+    // }
+
+    const auth = await easGoogle.auth.forGmail(senderId);
+
+    await easGoogle.gmail.users.messages.send({
+      auth,
+      userId: senderId,
+      requestBody: { raw },
+    }).catch(err => {
+      throw new GoogleApiError(err);
+    });
   }
-};
+}
