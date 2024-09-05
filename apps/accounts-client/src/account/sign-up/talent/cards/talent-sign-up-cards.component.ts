687f95f8329f1758e3758056c6e6bbb114a0fe67
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-09-06 02:21:42 +0530

    update swiper

--- apps/accounts-client/src/account/sign-up/talent/cards/talent-sign-up-cards.component.ts
+++ apps/accounts-client/src/account/sign-up/talent/cards/talent-sign-up-cards.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { NgSwiperModule } from 'app-shell/common/swiper/swiper.module';
+import { SwiperOptions } from 'swiper/types';
 
 @Component({
   standalone: true,
   templateUrl: './talent-sign-up-cards.component.html',
   styleUrl: './talent-sign-up-cards.component.less',
-  selector: 'talent-sign-up-cards'
+  selector: 'talent-sign-up-cards',
+  imports: [
+    NgSwiperModule,
+  ]
 })
 export class TalentSignUpCardsComponent {
-
+  protected readonly swiperOptions: SwiperOptions = {
+    autoplay: true,
+    loop: true
+  };
 }
