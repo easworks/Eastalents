9ed64d49e08f825ac50dbf1d5033f61d172b33d2
Dibyodyuti Mondal <dibyodyutimondalnfs@gmail.com>
2024-10-07 18:37:47 +0530

    add apis

--- libs/models/src/featured.ts
+++ libs/models/src/featured.ts
@@ -1,16 +1,6 @@
-import { Domain } from "./domain";
-import { SoftwareProduct } from "./software";
 
 export interface FeaturedDomain {
   domain: string;
   software: string[];
   roles: string[];
-}
-
-export interface updateDisplayFeatureProduct {
-  domainId: string;
-  domainName: string;
-  domainData: Domain[];
-  softareId?: string[];
-  softwareProduct?: SoftwareProduct[];
 }
