68f3146245c93f98678fbe062c3a2b0ba5542217
Abhishek <abhishek.policepatil@gmail.com>
2024-10-11 20:26:33 +0530

    update

--- apps/web-client/src/public/domains/domains.page.ts
+++ apps/web-client/src/public/domains/domains.page.ts
@@ -1,5 +1,7 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
 import { ImportsModule } from "@easworks/app-shell/common/imports.module";
+import { Domain } from "@easworks/models/domain";
+import { achievements } from "../common/achievements";
 
 @Component({
     standalone:true,
@@ -12,6 +14,8 @@ import { ImportsModule } from "@easworks/app-shell/common/imports.module";
 
 export class DomainsPageComponent{
 
+protected readonly domain$ = input.required<Domain>({alias:'domain'});
+protected readonly achievements = achievements;
 
 
 }
